15cefe02ee6b60bcd96af9cafadb939d633ff200
Pablo Roleri Lopez <rolerilopez@hotmail.com>
2021-08-05 14:28:15 +0200

    Creat models and links

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, length: {minimum: 6}
+  validates :movie, uniqueness: { scope: :list}
+  # tengo que validar que la movie es unica en la lista.
+  # esto lo hago atraves de "bookmark"
+  # valido que :movie, es unica: {en la lista}
 end
